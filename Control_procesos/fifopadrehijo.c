bce3e0b9659ed1a7ecaa625bd01d9b03c9b21cf5
Fran Lucena <fran.lucena@gmail.com>
2018-10-23 14:51:55 +0200

    Ejemplo fifo. El padre envia mensaje al Hijo

---
+++ Control_procesos/fifopadrehijo.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(){
+	int fd[2];
+	pid_t pid;
+	char saludoPadre[]="Pequeño saltamontes";
+	char buffer[80];
+
+
+	pipe(fd);
+
+	pid=fork();
+
+	switch(pid){
+		case -1: //error
+			printf("no se ha podido crear el hijo\n");
+			exit(-1);
+		case 0: 
+			close (fd[1]);
+			read (fd[0],buffer,sizeof(buffer));
+			printf("el hijo recibe un mensaje del pipe %s \n",saludoPadre);
+			break;
+		default: 
+			close (fd[0]);
+			write (fd[1],saludoPadre, strlen(saludoPadre));
+			read (fd[0],buffer,sizeof(buffer));
+			printf("el padre envia un mensaje al pipe %s\n",buffer);
+			wait(NULL);
+			break;
+	}
+}
